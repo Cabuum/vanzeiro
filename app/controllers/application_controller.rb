626f0f8d8f8848e6132720c648783f83a1110231
Joridos <jonatam.ribeiro@hotmail.com>
2014-12-09 10:51:56 -0200

    redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :redirect_subdomain
+
+ def redirect_subdomain
+ if request.host == 'www.vanzeiro.com'
+ redirect_to 'http://vanzeiro.com' + request.fullpath
+ end
+ end
 
 end
+
