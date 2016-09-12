e7ccca0a456dca7c1898147723fa861a1a968fbe
Bruno Casali <brunoocasali@gmail.com>
2016-09-12 11:45:08 -0300

    another basic improvements

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,13 +1,14 @@
 class DashboardsController < ApplicationController
   before_action :authenticate_user!
 
-  def index; end
+  def index
+	index_methods
+  end
 
   private
+  
   def index_methods
     @passengers = Passenger.where(user_id: current_user.id).count
-
     @movements = Movement.default current_user.id
-    p @movements.count
   end
 end
