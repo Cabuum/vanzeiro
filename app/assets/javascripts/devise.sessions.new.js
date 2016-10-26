55b47c4020a7920dea31a0ecc473cf8df37d0389
Joridos <joridoss@gmail.com>
2016-10-26 18:18:36 -0200

    fix .lazyload is not a function problem

--- app/assets/javascripts/devise.sessions.new.js
+++ app/assets/javascripts/devise.sessions.new.js
@@ -15,60 +15,64 @@ $(document).ready(function() {
         $('#form_register').show();
     });
 
-    $(".lazy").lazyload({
-        effect : "fadeIn"
-    });
+    if($.fn.lazyload) {
+        $(".lazy").lazyload({
+            effect : "fadeIn"
+        });
+    }
 
-    $('#form_login').validate({
+    if($.fn.validate) {
+        $('#form_login').validate({
 
-        errorElement: 'span',
-        errorClass: 'error',
-        focusInvalid: false,
-        ignore: "",
-        rules: {
-            login_email: {
-                minlength: 2,
-                required: true,
-                email: true
+            errorElement: 'span',
+            errorClass: 'error',
+            focusInvalid: false,
+            ignore: "",
+            rules: {
+                login_email: {
+                    minlength: 2,
+                    required: true,
+                    email: true
+                },
+                login_pwd: {
+                    minlength: 2,
+                    required: true
+                },
+                gendericonic: {
+                    required: true
+                }
             },
-            login_pwd: {
-                minlength: 2,
-                required: true
+
+            invalidHandler: function (event, validator) {
+                //display error alert on form submit
             },
-            gendericonic:{
-                required: true
+
+            errorPlacement: function (error, element) { // render error placement for each input type
+
+                var icon = $(element).parent('.input-with-icon').children('i');
+                var parent = $(element).parent('.input-with-icon');
+                icon.removeClass('fa fa-check').addClass('fa fa-exclamation');
+                parent.removeClass('success-control').addClass('error-control');
+            },
+
+            highlight: function (element) { // hightlight error inputs
+                var parent = $(element).parent();
+                parent.removeClass('success-control').addClass('error-control');
+            },
+
+            unhighlight: function (element) { // revert the change done by hightlight
+            },
+
+            success: function (label, element) {
+                var icon = $(element).parent('.input-with-icon').children('i');
+                var parent = $(element).parent('.input-with-icon');
+                icon.removeClass("fa fa-exclamation").addClass('fa fa-check');
+                parent.removeClass('error-control').addClass('success-control');
+            },
+
+            submitHandler: function (form) {
+                $(form).ajaxSubmit();
             }
-        },
-
-        invalidHandler: function (event, validator) {
-            //display error alert on form submit
-        },
-
-        errorPlacement: function (error, element) { // render error placement for each input type
-
-            var icon = $(element).parent('.input-with-icon').children('i');
-            var parent = $(element).parent('.input-with-icon');
-            icon.removeClass('fa fa-check').addClass('fa fa-exclamation');
-            parent.removeClass('success-control').addClass('error-control');
-        },
-
-        highlight: function (element) { // hightlight error inputs
-            var parent = $(element).parent();
-            parent.removeClass('success-control').addClass('error-control');
-        },
-
-        unhighlight: function (element) { // revert the change done by hightlight
-        },
-
-        success: function (label, element) {
-            var icon = $(element).parent('.input-with-icon').children('i');
-            var parent = $(element).parent('.input-with-icon');
-            icon.removeClass("fa fa-exclamation").addClass('fa fa-check');
-            parent.removeClass('error-control').addClass('success-control');
-        },
-
-        submitHandler: function (form) {
-            $(form).ajaxSubmit();
-        }
-    });
+        });
+    }
 });
