82c1f0a0a72b3f115ccacd8e22d6e107ecd3cb4d
Bruno Casali <brunoocasali@gmail.com>
2016-10-27 01:31:50 -0200

    Try to fix problem with amazon aws in test

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -22,16 +22,16 @@ class NullStorage
 end
 
 CarrierWave.configure do |config|
-  config.fog_credentials = {
-    provider:              'AWS',
-    aws_access_key_id:     ENV['S3_ACCESS_KEY'],
-    aws_secret_access_key: ENV['S3_SECRET_ACCESS_KEY'],
-    region:                'us-west-1',
-    path_style: true
-  }
-  config.fog_directory = ENV['S3_BUCKET_NAME']
-
   if Rails.env.production?
+    config.fog_credentials = {
+      provider:              'AWS',
+      aws_access_key_id:     ENV['S3_ACCESS_KEY'],
+      aws_secret_access_key: ENV['S3_SECRET_ACCESS_KEY'],
+      region:                'us-west-1',
+      path_style: true
+    }
+
+    config.fog_directory = ENV['S3_BUCKET_NAME']
     config.storage = :fog
   elsif Rails.env.development?
     config.storage = :file
