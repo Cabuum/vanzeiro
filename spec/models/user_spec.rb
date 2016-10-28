5ae0832200510989ae77bd388834bc5f82e748c7
Bruno Casali <brunoocasali@gmail.com>
2016-10-28 01:24:37 -0200

    Install simple_form

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,18 +4,13 @@ RSpec.describe User, type: :model do
   context 'associations' do
     it { is_expected.to have_many(:authorizations) }
     it { is_expected.to have_many(:bank_accounts) }
+
     it { is_expected.to have_one(:setting).class_name(Setting) }
   end
 
   context 'model validations' do
     it { is_expected.to validate_presence_of(:email) }
     it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
-
-    # it { is_expected.to validate_presence_of(:role) }
-    # it { is_expected.to validate_presence_of(:profile) }
-
-    # it { is_expected.to validate_presence_of(:status) }
-    # it { is_expected.to validate_inclusion_of(:status).in_array(CommonStatus.list) }
   end
 
   context 'table fields' do
@@ -23,12 +18,6 @@ RSpec.describe User, type: :model do
     it { is_expected.to have_db_column(:name).of_type(:string) }
     it { is_expected.to have_db_column(:encrypted_password).of_type(:string) }
     it { is_expected.to have_db_column(:reset_password_token).of_type(:string) }
-
-    # it { is_expected.to have_db_column(:status).of_type(:integer) }
-    # it { is_expected.to have_db_column(:role_id).of_type(:integer) }
-    # it { is_expected.to have_db_column(:profile_id).of_type(:integer) }
-
-    # it { is_expected.to have_db_column(:is_admin).of_type(:boolean) }
   end
 
   context 'table indexes' do
